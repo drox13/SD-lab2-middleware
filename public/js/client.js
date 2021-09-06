f833b997cce8ea0fb3f48c2d11c9026fb6adbb2e
Samuel López <samuel.lopez@uptc.edu.co>
2021-09-06 12:44:33 -0500

    image rendering client-side working

--- public/js/client.js
+++ public/js/client.js
@@ -3,15 +3,28 @@ window.onload = function () {
 }
 
 function upload() {
-    let image = document.getElementById('imageSelector').files[0]; 
+    let image = document.getElementById('imageSelector').files[0];
     const xhr = new XMLHttpRequest();
     xhr.open('POST', 'http://localhost:8000/', true);
-        xhr.onreadystatechange = function() {
-            if ( 4 == this.readyState ) {
-                console.log(xhr.response);
-            }
-        };
-        var formData = new FormData();
-        formData.append("image", image);
-        xhr.send(formData);
+    xhr.onreadystatechange = function () {
+        if (4 == this.readyState) {
+            let canvas = document.getElementById('myCanvas')
+            let data = JSON.parse(xhr.response)
+            var ctx = canvas.getContext("2d");
+            var imgData = ctx.createImageData(data.width, data.height);
+            canvas.width = data.width;
+            canvas.height = data.height;
+            var i;
+        for (i = 0; i < imgData.data.length; i += 4) {
+            imgData.data[i+0] = data.pixels.data[i+0];
+            imgData.data[i+1] = data.pixels.data[i+1];
+            imgData.data[i+2] = data.pixels.data[i+2];
+            imgData.data[i+3] = data.pixels.data[i+3];
+        }
+        ctx.putImageData(imgData,0,0); 
+        }
+    };
+    var formData = new FormData();
+    formData.append("image", image);
+    xhr.send(formData);
 }
